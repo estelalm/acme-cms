a1edf04d7393848e0a386ba096024657bad4262c
Estela Alves de Moraes <esteela.almoraes@gmail.com>
2024-05-01 23:52:00 -0300

    login

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,73 @@
 
 const botaoEntrar = document.getElementById('botao-entrar')
 
-botaoEntrar.addEventListener('click', () =>{
+const getAdmnistradores = async function () {
 
-    window.location.assign('./src/pages/dashboard.html')
+    try{
+        const url = 'http://localhost:8080/v2/AcmeFilmes/admnistradores'
+        const response = await fetch(url)
+        const data = await response.json()
+    
+        console.log(data)
+    
+        return data.admnistradores
+    }catch(error){
+        console.log(error)
+        return false
+    }
+}
 
-})
+const logar = async function () {
+
+    
+    let admnistradores = await getAdmnistradores()
+    let loginValido = false
+
+    let login = document.getElementById('email').value
+    let senha = document.getElementById('senha').value
+
+    console.log(login, senha)
+    admnistradores.forEach((admnistrador) => {
+
+        let admnistradorEmail
+        let admnistradorSenha
+
+        if (admnistrador.login == login && senha == admnistrador.senha) {
+            admnistradorEmail = admnistrador.login
+            admnistradorSenha = admnistrador.senha
+            let idAdmnistrador = admnistrador.id
+            localStorage.setItem('idAdmnistrador', idAdmnistrador)
+            loginValido = true
+        } 
+    })
+
+    if(loginValido){
+        window.location.assign('/src/pages/dashboard.html')
+    }else{
+        alert('Usuário ou senha incorretos')
+    }
+    
+}
+
+botaoEntrar.addEventListener('click', logar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//     "id": 1,
+//     "nome": "admin",
+//     "login": "admin@acmefilmes.com",
+//     "senha": "adm123"
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,29 @@ module.exports = {
       'inder': ['"Inder"', 'sans-serif']
     }
   },
-  plugins: [],
+  theme: {
+    extend: {
+      colors:{
+        'rosa-claro': '#E0B1CB',
+        'rosa-muito-claro': '#EAD0DE',
+        'rosa-cinzento': '#BE95C4',
+        'roxo-claro': '#9F86C0',
+        'roxo': '#5B3B85',
+        'roxo-escuro-1': '#271C48',
+        'roxo-escuro2': '#231942',
+        'roxo-transparencia': '#3A285E'
+      },
+      backgroundImage:{
+        'fundo-geral': 'linear-gradient(#8A6FAE, #5B3B85)'
+      }
+    },
+    fontFamily: {
+      'textos': ['"Inder"', 'sans-serif']
+    }
+  },
+  plugins: [
+    // ...
+    require('tailwind-scrollbar')
+]
 }
 
